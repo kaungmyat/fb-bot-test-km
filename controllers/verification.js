5addce86f2a86bfed457b2b83e829f27a0dc191a
KAUNG MYAT <kaung.myat49@gmail.com>
2018-09-02 21:27:08 +0800

    commit

--- controllers/verification.js
+++ controllers/verification.js
@@ -1,7 +1,7 @@
 module.exports = (req, res) => {
     const hubChallenge = req.query['hub.challenge'];
     const hubMode = req.query['hub.mode'];
-    const verifyTokenMatches = (req.query['hub.verify_token'] === 'crowdbotics');
+    const verifyTokenMatches = (req.query['hub.verify_token'] === 'WHATISYOURDADDY');
     if (hubMode && verifyTokenMatches) {
         res.status(200).send(hubChallenge);
     } else {
